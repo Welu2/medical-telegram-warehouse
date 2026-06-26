e884d67ac33d57e868b50ddb21ee0107cc72b606
Welu12 <welelabekele5@gmail.com>
2026-06-26 17:07:02 +0300

    feat: add scraper configuration

---
+++ src/config.py
@@ -0,0 +1,13 @@
+"""
+Project configuration.
+"""
+
+CHANNELS = [
+    "tikvahpharma",
+    "CheMed123",
+    "lobelia4cosmetics",
+]
+
+RAW_DATA_DIR = "data/raw/telegram_messages"
+IMAGE_DIR = "data/raw/images"
+LOG_DIR = "logs"
